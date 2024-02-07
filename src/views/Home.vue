48cfda693f55fc7b92644b692557a4e080e28d76
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-02-07 21:21:35 +0100

    screen for mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
-    <video autoplay muted loop id="myVideo">
-      <source src="..\..\video\v1.mp4" type="video/mp4" />
-      Your browser does not support HTML5 video.
-    </video>
-  
-    <button class="content">
+
+    <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+      <button class="relative z-30 p-5 text-8xl text-white">
       <router-link to="first">trip</router-link>
     </button>
+
+      <video autoplay muted loop class="absolute z-10 w-auto min-w-full min-h-full max-w-none" id="myVideo">
+        <source src="..\..\video\v1.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+    </header>
+  
+    
   </template>
   
   <style>
